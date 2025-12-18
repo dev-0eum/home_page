38db9dc6f3f1c8667edf3547aa88826ccadfe45f
JunYeob <105794285+dev-0eum@users.noreply.github.com>
2025-12-18 08:56:04 +0000

    [Management] Update Admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -7,5 +7,5 @@ from accounts.models import Alumini
 
 @admin.register(Alumini)
 class AluminiAdmin(admin.ModelAdmin):
-    list_display = ['name', 'created_at']
-    list_display_links = ['name', 'created_at']
+    list_display = ['pk', 'user','name','th','company','contact','introduce','created_at']
+    list_display_links = ['pk', 'user','name','th','company','contact','introduce','created_at']
--- mainpg/admin.py
+++ mainpg/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 
-
+from .models import Question, Answer, Category
 
 # Register your models here.
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['pk','title','author','category', 'created_at']
+    list_display_links = ['pk', 'title','author','category', 'created_at']
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'question', 'author', 'created_at']
+    list_display_links = ['pk', 'question', 'author', 'created_at']
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'slug']
+    list_display_links = ['pk', 'name', 'slug']
